b9ecdb054ad02044cf2b07e47a47a2d01358f28c
ravil <ravilshulayakov@gmail.com>
2024-02-27 21:25:59 +0300

    Hello from HTML

--- src/main/java/ru/ravil/myapp/MyAppApplication.java
+++ src/main/java/ru/ravil/myapp/MyAppApplication.java
@@ -2,6 +2,7 @@ package ru.ravil.myapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MyAppApplication {
@@ -9,5 +10,4 @@ public class MyAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MyAppApplication.class, args);
 	}
-
 }
